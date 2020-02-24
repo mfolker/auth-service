5023a6fec3b0ef19e0fece54bbbd1d95e2464e4a
Matthew Folker <matthew.folker@gmail.com>
2020-02-24 21:39:07 +0000

    Public resources path, views and view engine

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const express = require('express');
 const log4js = require('log4js');
 const httpContext = require('express-http-context');
 const uuidv4 = require('uuid/v4');
+const path = require('path');
 const { Provider } = require('oidc-provider');
-
 
 const logging = require('./logging');
 //Move out into an initialiser class.
@@ -30,6 +30,10 @@ app.use((req, res, next) => {
 	httpContext.set(logging.correlationIdContextKey, uuidv4());
 	next();
 });
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	logger.info('Hello world requested.');
