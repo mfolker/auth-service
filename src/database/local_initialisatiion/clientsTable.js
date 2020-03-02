2f219d60a1c0296af856b86f681d9537bf1db3be
Matthew Folker <matthew.folker@gmail.com>
2020-03-02 12:16:07 +0000

    Update clientsTable.js

--- src/database/local_initialisatiion/clientsTable.js
+++ src/database/local_initialisatiion/clientsTable.js
@@ -1,25 +1,37 @@
-var AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 AWS.config.update({
-	endpoint: 'http://localhost:8000'
+  endpoint: "http://localhost:8000"
 });
-var dynamodb = new AWS.DynamoDB();
+const dynamodb = new AWS.DynamoDB();
 
-var params = {
-	TableName: 'Clients',
-	KeySchema: [
-		{ AttributeName: 'id', KeyType: 'HASH' } //Partition key
-	],
-	AttributeDefinitions: [ { AttributeName: 'id', AttributeType: 'N' } ],
-	ProvisionedThroughput: {
-		ReadCapacityUnits: 5,
-		WriteCapacityUnits: 5
-	}
+const tableName = "Clients";
+
+const params = {
+  TableName: tableName,
+  KeySchema: [
+    { AttributeName: "id", KeyType: "HASH" } //Partition key
+  ],
+  AttributeDefinitions: [{ AttributeName: "id", AttributeType: "N" }],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  }
 };
 
 dynamodb.createTable(params, function(err, data) {
-	if (err) {
-		console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
-	} else {
-		console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
-	}
+  if (err) {
+    if (err.code === "ResourceInUseException") {
+      console.log(`Table ${tableName} already exists.`);
+    } else {
+      console.error(
+        `Unable to create table: ${tableName}. Error JSON:`,
+        JSON.stringify(err, null, 2)
+      );
+    }
+  } else {
+    console.log(
+      `Created table: ${tableName}. Table description JSON:`,
+      JSON.stringify(data, null, 2)
+    );
+  }
 });
